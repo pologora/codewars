85d191b016791a3e4dc68a86e2b2c2c199c3146a
Oleksandr <lysakova@yahoo.com>
2022-11-17 18:06:58 +0100

    Your order, please

--- 6kyo/app.js
+++ 6kyo/app.js
@@ -1,9 +1,1 @@
-function getMiddle(s) {
-  const stringLength = s.length;
-  const middleChar = s[Math.floor(stringLength / 2)];
-  const twoMiddleChars =
-    s[Math.floor(stringLength / 2) - 1] + s[Math.floor(stringLength / 2)];
-  return s.length % 2 ? middleChar : twoMiddleChars;
-}
 
-console.log(getMiddle('abcdem'));
---
+++ 6kyo/your_order_pls.js
@@ -0,0 +1,33 @@
+// Your task is to sort a given string. Each word in the string will contain a single number.
+//  This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+// Examples
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+function order(words) {
+  const array = words.split(' ');
+  const reduce = array.reduce((acc, word) => {
+    const number = word.match(/[0-9]/);
+    return { ...acc, [number]: word };
+  }, {});
+  return Object.values(reduce).join(' ');
+}
+const str1 = 'is2 Thi1s T4est 3a';
+const str2 = '4of Fo1r pe6ople g3ood th5e the2';
+
+console.log(order(str1));
+
+//   top/
+function order(words) {
+  return words
+    .split(' ')
+    .sort(function (a, b) {
+      return a.match(/\d/) - b.match(/\d/);
+    })
+    .join(' ');
+}
