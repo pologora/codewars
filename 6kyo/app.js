a5a5506b904dbbb52b7ce833b37f367e1cff029c
Oleksandr <lysakova@yahoo.com>
2022-11-29 17:29:30 +0100

    Tribonacci

--- 6kyo/app.js
+++ 6kyo/app.js
@@ -1,13 +1,12 @@
-function mostFrequentItemCount(collection) {
-  if (!collection.length) return 0;
-  const result = collection.reduce((acc, num) => {
-    const count = acc[num] ?? 0;
-    return {
-      ...acc,
-      [num]: count + 1,
-    };
-  }, {});
-  return Math.max(...Object.values(result));
+function minMax(arr) {
+  return arr.reduce(
+    (acc, num) => {
+      acc[0] > num && (acc[0] = num);
+      acc[1] < num && (acc[1] = num);
+      return acc;
+    },
+    [arr[0], arr[0]]
+  );
 }
 
-console.log(mostFrequentItemCount([1]));
+console.log(minMax([1, 2, 3, 4, 5, 6]));
