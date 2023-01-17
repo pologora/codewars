388bc8b44f7bcb9915d8ed05d034d59d94eceaec
Oleksandr <lysakova@yahoo.com>
2023-01-17 08:55:47 +0100

    is isomorphic

--- 6kyo/app.js
+++ 6kyo/app.js
@@ -1,38 +1,10 @@
-class User {
-  constructor(id, name) {
-    this.id = id;
-    this.name = name;
+function solution(number) {
+  let sum = 0;
+  for (let i = 3; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) sum += i;
   }
 
-  hasAccess() {
-    return this.name === 'Bob';
-  }
+  return sum;
 }
 
-class NullUser {
-  constructor() {
-    this.id = -1;
-    this.name = 'Guest';
-  }
-
-  hasAccess() {
-    return false;
-  }
-}
-
-const users = [new User(1, 'Bob'), new User(2, 'John')];
-
-function getUser(id) {
-  const user = users.find((user) => user.id === id);
-  return user ? user : new NullUser();
-}
-
-function printUser(id) {
-  const user = getUser(id);
-
-  console.log('Hello ' + user.name);
-  
-  user.hasAccess()
-    ? console.log('You have access')
-    : console.log('You are not allowed here');
-}
+console.log(solution(10));
